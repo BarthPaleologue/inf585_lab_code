f6a125f3da16170f0542cf567c0e0121bd84f651
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-02-26 16:34:28 +0100

    stable fluids

--- scenes_inf585/11_stable_fluids/src/simulation/simulation.cpp
+++ scenes_inf585/11_stable_fluids/src/simulation/simulation.cpp
@@ -3,9 +3,8 @@
 using namespace cgp;
 
 
-
-void divergence_free(grid_2D<vec2>& new_velocity, grid_2D<vec2> const& velocity, grid_2D<float>& divergence, grid_2D<float>& gradient_field)
-{
+void divergence_free(grid_2D<vec2> &new_velocity, grid_2D<vec2> const &velocity, grid_2D<float> &divergence,
+                     grid_2D<float> &gradient_field) {
     // v = projection of v0 on divergence free vector field
     //
     // v : Final vector field to be filled
@@ -18,5 +17,37 @@ void divergence_free(grid_2D<vec2>& new_velocity, grid_2D<vec2> const& velocity,
     // 1. Compute divergence of v0
     // 2. Compute gradient_field such that nabla(gradient_field)^2 = div(v0)
     // 3. Compute v = v0 - nabla(gradient_field)
+
+    int const N = int(velocity.dimension.x);
+
+    for (int k = 0; k < 10; k++) {
+        // compute divergences
+        for(int x = 1; x < N - 1; ++x) {
+            for(int y = 1; y < N - 1; ++y) {
+                divergence(x, y) = (velocity(x + 1, y).x - velocity(x - 1, y).x +
+                                    velocity(x, y + 1).y - velocity(x, y - 1).y) / 2.0f;
+            }
+        }
+
+        // compute gradients
+        for (int x = 1; x < N - 1; ++x) {
+            for (int y = 1; y < N - 1; ++y) {
+                float divergence_v0 = divergence(x, y);
+
+                gradient_field(x, y) = (gradient_field(x - 1, y) + gradient_field(x + 1, y) +
+                                        gradient_field(x, y - 1) + gradient_field(x, y + 1) -
+                                        divergence_v0) / 4.0f;
+            }
+        }
+    }
+
+    // compute final velocity
+    for (int x = 1; x < N - 1; ++x) {
+        for (int y = 1; y < N - 1; ++y) {
+            vec2 nabla_gradient_field = vec2(gradient_field(x + 1, y) - gradient_field(x - 1, y),
+                                             gradient_field(x, y + 1) - gradient_field(x, y - 1)) / 2.0f;
+            new_velocity(x, y) = velocity(x, y) - nabla_gradient_field;
+        }
+    }
 
 }
--- scenes_inf585/11_stable_fluids/src/simulation/simulation.hpp
+++ scenes_inf585/11_stable_fluids/src/simulation/simulation.hpp
@@ -4,19 +4,21 @@
 #include "boundary.hpp"
 
 
+void divergence_free(cgp::grid_2D<cgp::vec2> &new_velocity, cgp::grid_2D<cgp::vec2> const &velocity,
+                     cgp::grid_2D<float> &divergence, cgp::grid_2D<float> &gradient_field);
+
+template<typename T>
+void diffuse(cgp::grid_2D<T> &new_field, cgp::grid_2D<T> const &field_reference, float mu, float dt,
+             boundary_condition boundary);
+
+template<typename T>
+void advect(cgp::grid_2D<T> &new_value, cgp::grid_2D<T> const &value_reference, cgp::grid_2D<cgp::vec2> const &velocity,
+            float dt);
 
 
-void divergence_free(cgp::grid_2D<cgp::vec2>& new_velocity, cgp::grid_2D<cgp::vec2> const& velocity, cgp::grid_2D<float>& divergence, cgp::grid_2D<float>& gradient_field);
-
-template <typename T> void diffuse(cgp::grid_2D<T>& new_field, cgp::grid_2D<T> const& field_reference, float mu, float dt, boundary_condition boundary);
-template <typename T> void advect(cgp::grid_2D<T>& new_value, cgp::grid_2D<T> const& value_reference, cgp::grid_2D<cgp::vec2> const& velocity, float dt);
-
-
-
-
-template <typename T>
-void diffuse(cgp::grid_2D<T>& new_field, cgp::grid_2D<T> const& field_reference, float mu, float dt, boundary_condition boundary)
-{
+template<typename T>
+void diffuse(cgp::grid_2D<T> &new_field, cgp::grid_2D<T> const &field_reference, float mu, float dt,
+             boundary_condition boundary) {
     using namespace cgp;
     // Compute diffusion on f
     //  Use f as current value, f_prev as previous value
@@ -27,38 +29,52 @@ void diffuse(cgp::grid_2D<T>& new_field, cgp::grid_2D<T> const& field_reference,
     //  Use Gauss-Seidel iterations over 10 to 15 times
     //   For each iteration, you may reset the boundary conditions with the following code:
     //   		if(boundary==copy)
-	//		set_boundary(new_field);
-	//       	else
-	//		set_boundary_reflective(new_field);
+    //		set_boundary(new_field);
+    //       	else
+    //		set_boundary_reflective(new_field);
 
+    int const N = int(new_field.dimension.x);
 
+    for (int k = 0; k < 10; ++k) {
+        for (int x = 1; x < N - 1; ++x) {
+            for (int y = 1; y < N - 1; ++y) {
+                new_field(x, y) = (field_reference(x, y) + mu * dt * (new_field(x - 1, y) + new_field(x + 1, y) +
+                                                                    new_field(x, y - 1) + new_field(x, y + 1))) /
+                                  (1.0f + 4.0f * mu * dt);
+            }
+        }
+        if (boundary == copy) {
+            set_boundary(new_field);
+        } else {
+            set_boundary_reflective(new_field);
+        }
+    }
 
 }
 
 
-
-template <typename T>
-void advect(cgp::grid_2D<T>& new_value, cgp::grid_2D<T> const& value_reference, cgp::grid_2D<cgp::vec2> const& velocity, float dt)
-{
+template<typename T>
+void advect(cgp::grid_2D<T> &new_value, cgp::grid_2D<T> const &value_reference, cgp::grid_2D<cgp::vec2> const &velocity,
+            float dt) {
     using namespace cgp;
     // Compute advection of value along the velocity v, given its previous state value_prev
     int const N = int(new_value.dimension.x);
-	
-	for(int x=1; x<N-1; ++x) {
-        for(int y=1; y<N-1; ++y) {
-            vec2 const p  = {float(x),float(y)};
+
+    for (int x = 1; x < N - 1; ++x) {
+        for (int y = 1; y < N - 1; ++y) {
+            vec2 const p = {float(x), float(y)};
 
             // retrieve an averaged velocity value around the position p to avoid grid artifacts
-			vec2 const v = 0.25f*(interpolation_bilinear(velocity, x+0.5f, y+0.5f)+
-				interpolation_bilinear(velocity, x-0.5f, y+0.5f)+
-				interpolation_bilinear(velocity, x+0.5f, y-0.5f)+
-				interpolation_bilinear(velocity, x-0.5f, y-0.5f));
+            vec2 const v = 0.25f * (interpolation_bilinear(velocity, x + 0.5f, y + 0.5f) +
+                                    interpolation_bilinear(velocity, x - 0.5f, y + 0.5f) +
+                                    interpolation_bilinear(velocity, x + 0.5f, y - 0.5f) +
+                                    interpolation_bilinear(velocity, x - 0.5f, y - 0.5f));
 
             // Back tracing
-			vec2 const p_back = p-dt*v;
+            vec2 const p_back = p - dt * v;
 
             // Retrieve back-traced value and set it to the new value
-			new_value(x,y) = interpolation_bilinear(value_reference, p_back.x, p_back.y);
+            new_value(x, y) = interpolation_bilinear(value_reference, p_back.x, p_back.y);
 
         }
     }
